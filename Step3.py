71b089b3495f39c25c316d18e13dde7c59826f6e
plazar23 <plazar23@gmail.com>
2018-02-17 03:45:31 +0100

    popravka

--- Step3.py
+++ Step3.py
@@ -7,8 +7,8 @@ reci8slova = open('reci8slova.txt', 'w', encoding="utf-8")
 reci9slova = open('reci9slova.txt', 'w', encoding="utf-8")
 reci10slova = open('reci10slova.txt', 'w', encoding="utf-8")
 reci11slova = open('reci11slova.txt', 'w', encoding="utf-8")
-reci12slova = open('reci12slova', 'w', encoding="utf-8")
-ostalereci = open('osatlereci', 'w', encoding="utf-8")
+reci12slova = open('reci12slova.txt', 'w', encoding="utf-8")
+ostalereci = open('osatlereci.txt', 'w', encoding="utf-8")
 
 pomocnalista = list()
 pomocnistring = str()
@@ -18,41 +18,51 @@ for line in recnik2:                                              # Ravzvrstavam
     pomocnalista = line.split()
     if int(pomocnalista[1]) < 4 or int(pomocnalista[1]) > 12:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         ostalereci.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 4:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci4slova.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 5:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci5slova.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 6:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci6slova.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 7:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci7slova.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 8:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci8slova.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 9:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci9slova.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 10:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci10slova.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 11:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci11slova.write(pomocnistring + '\n')
     elif int(pomocnalista[1]) == 12:
         del pomocnalista[1]
+        pomocnalista.reverse()
         pomocnistring = str(' '.join(pomocnalista[:]))
         reci12slova.write(pomocnistring + '\n')
